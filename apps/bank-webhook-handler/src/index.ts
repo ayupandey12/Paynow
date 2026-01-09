8fb60509df33f65ca0b6b15059e665cce1550620
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-09 08:31:38 +0530

    updated bankwebhook server

--- apps/bank-webhook-handler/src/index.ts
+++ apps/bank-webhook-handler/src/index.ts
@@ -2,20 +2,39 @@
 import { prisma } from "@repo/db";
 import express from "express"
 const app =express();
-
+app.use(express.json());
 
 app.post('/bankserver',async (req,res)=>{
-    const {userID,token,amount}=req.body;
-    await prisma.$transaction([
+    const {userID,token,amount}:{userID:string,token:string,amount:string}=req.body;
+   try {
+     await prisma.$transaction([
          prisma.balance.updateMany({
             where:{
                 userID:userID
             },
             data:{
                 amount:{
-                    increment:amount
+                    increment:Number(amount)
                 }
+            }
+        }),
+        prisma.onRampTransaction.update({
+            where:{
+                token:token
+            },
+            data:{
+                status:"Success"
             }
         })
     ])
+      res.json({
+            message: "Captured"
+        })
+   } catch (e) {
+      console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+   }
 })
+   app.listen(3010);
