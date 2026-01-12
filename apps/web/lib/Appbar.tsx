1288d1fecef8254d03dd892e048406fc72cef6f1
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-12 22:36:23 +0530

    updated appbar

--- apps/web/lib/Appbar.tsx
+++ apps/web/lib/Appbar.tsx
@@ -1,14 +1,13 @@
 "use client"
 import {Appbar} from "@repo/ui/Appbar"
 import { signIn,signOut,useSession } from "next-auth/react"
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export const Appbarprovider=()=>{
     const session=useSession();
-    const route=useRouter();
     return <div>
         <Appbar onSignin={signIn} onSignout={async ()=>{ await signOut({redirect:false}) //should use redirect to false so not to send callback url
-        route.push('/api/auth/signin')
+        redirect('/api/auth/signin')
     }} user={session.data?.user}/>
     </div>
 }
