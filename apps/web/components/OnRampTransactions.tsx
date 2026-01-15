1997249415b69580f63171e361caf551030757e2
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-15 07:54:49 +0530

    added balancecard and onramptransactionscard.tsx

---
+++ apps/web/components/OnRampTransactions.tsx
@@ -0,0 +1,48 @@
+import { Card } from "@repo/ui/card"
+export enum StatusProps {
+    Success = "Success",
+    Processing = "Processing",
+    Failed = "Failed"
+}
+
+interface Transaction {
+    time: Date;
+    amount: number;
+    status: StatusProps;
+    provider: string;
+}
+export const OnRampTransactions = ({transactions}:{transactions:Transaction[]}) => {
+    if (!transactions.length) {
+        return <Card title="Recent Transactions">
+            <div className="text-center pb-8 pt-8">
+                No Recent transactions
+            </div>
+        </Card>
+    }
+    return <Card title="Recent Transactions">
+        <div className="pt-2">
+            {transactions.map(t => <div className="flex justify-between">
+                <div>
+                    <div className="text-sm">
+                        Received INR
+                    </div>
+                    <div className="text-slate-600 text-xs">
+                        {t.time.toDateString()}
+                    </div>
+                </div>
+                     <div className="flex flex-col justify-center text-right">
+                            <span className="font-semibold text-sm">
+                                + Rs {t.amount / 100}
+                            </span>
+                            <span className={`text-[10px] uppercase font-bold ${
+                                t.status === StatusProps.Success ? 'text-green-600' : 'text-yellow-600'
+                            }`}>
+                                {t.status}
+                            </span>
+                     </div>
+
+
+            </div>)}
+        </div>
+    </Card>
+}
