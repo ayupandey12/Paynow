fb7e7e9d8bec3f09a3b276d1838e84d659e03af1
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-15 08:24:33 +0530

    updated transferpage

--- apps/web/app/(dashboard)/transfer/page.tsx
+++ apps/web/app/(dashboard)/transfer/page.tsx
@@ -1,5 +1,36 @@
+import { getServerSession } from "next-auth";
 import { AddMoneyCard } from "../../../components/AddMoneyCard";
-
-export default function transfer(){
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { prisma } from "@repo/db";
+ 
+export default async function transfer(){
+  async function getbalance() {
+    const session=await getServerSession(authOptions);
+    const balance=await prisma.balance.findFirst({
+      where:{
+        userID:session?.user?.id
+      }
+    })
+    return {
+      amount:balance?.amount||0,
+      locked:balance?.locked||0
+    }
+  }
+  async function getonramptransactions() {
+    const session=await getServerSession(authOptions);
+    const transactions=await prisma.onRampTransaction.findMany({
+      where:{
+        userID:session?.user?.id
+      }
+    })
+    return transactions.map(t => ({
+        time: t.startTime,
+        amount: t.amount,
+        status: t.status,
+        provider: t.provider
+    }))
+  }
+ await getbalance();
+ await getonramptransactions()
   return <AddMoneyCard/>
 }
