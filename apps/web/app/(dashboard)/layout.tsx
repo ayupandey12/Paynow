5905613cf373d52de978aceddc2bbef660669cfa
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-18 08:20:43 +0530

    added p2p page on sidebar in layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -12,6 +12,7 @@ export default function Layout({
                 <Sidebar href={"/dashboard"} icon={<HomeIcon />} title="Home" />
                 <Sidebar href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
                 <Sidebar href={"/transactions"} icon={<TransactionsIcon />} title="Transactions" />
+                <Sidebar href={"/p2p"} icon={<P2PTransferIcon/>} title="P2P" />
             </div>
         </div>
             {children}
@@ -36,4 +37,9 @@ function TransactionsIcon() {
     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
   </svg>
   
+}
+function P2PTransferIcon() {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+  </svg>
 }
