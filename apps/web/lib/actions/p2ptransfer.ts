77b1854c890d2235893bb6b71cda2dffb7e0f228
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-19 08:48:54 +0530

    updated p2ptransfer action with lock row

--- apps/web/lib/actions/p2ptransfer.ts
+++ apps/web/lib/actions/p2ptransfer.ts
@@ -3,41 +3,51 @@ import { prisma } from "@repo/db";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../app/api/auth/[...nextauth]/route";
 
-export async function P2pTransfer({to,amount}:{to:string,amount:number}) {
+export async function P2pTransfer({ to, amount }: { to: string, amount: number }) {
     const session = await getServerSession(authOptions);
     const from = session?.user?.id;
+
     if (!from) {
-        return {
-            message: "Error while sending"
-        }
+        return { message: "Unauthorized" };
     }
-    const toUser = await prisma.user.findFirst({
-        where: {
-            phone: to
-        }
+    const toUser = await prisma.user.findUnique({
+        where: { phone: to }
     });
 
     if (!toUser) {
-        return {
-            message: "User not found"
-        }
+        return { message: "Recipient user not found" };
     }
-    await prisma.$transaction(async (tx) => {
-        const fromBalance = await tx.balance.findFirst({
-            where: { userID: from },
-          });
-          if (!fromBalance || fromBalance.amount < amount) {
-            return {message:"Insufficient funds"}
-          }
 
-          await tx.balance.updateMany({
-            where: { userID: from },
-            data: { amount: { decrement: amount } },
-          });
+    try {
+        await prisma.$transaction(async (tx) => {
+            await tx.$executeRaw`SELECT * FROM "Balance" WHERE "userID" = ${from} FOR UPDATE`;
 
-          await tx.balance.updateMany({
-            where: { userID: toUser.id },
-            data: { amount: { increment: amount } },
-          });
-    });
+            const fromBalance = await tx.balance.findFirst({
+                where: { userID: from },
+            });
+
+            if (!fromBalance || fromBalance.amount < amount) {
+                throw new Error("Insufficient funds");
+            }
+
+            await tx.balance.updateMany({
+                where: { userID: from },
+                data: { amount: { decrement: amount } },
+            });
+
+            await tx.balance.updateMany({
+                where: { userID: toUser.id },
+                data: { amount: { increment: amount } },
+            });
+        },
+             {
+        timeout: 30000, // 30 seconds
+        });
+
+        return { message: "Transfer successful" };
+    } catch (e: any) {
+        return {
+            message: e.message || "Error while processing transfer"
+        };
+    }
 }
