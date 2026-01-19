c21535b612dcb1905c861090a0fb568420738d5f
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-19 22:50:58 +0530

    updated text ui and p2pcard for setmessage

--- apps/web/components/P2Pcard.tsx
+++ apps/web/components/P2Pcard.tsx
@@ -8,7 +8,9 @@ import { P2pTransfer } from "../lib/actions/p2ptransfer"
 export const P2Pcard=()=>{
     const [amount,setamount]=useState<number|null>()
     const [phone,serphone]=useState<string|null>()
+    const [message,setmessage]=useState<String>()
     useEffect(() => {
+        setmessage("")
       console.log(amount,phone)
     }, [amount,phone])
     
@@ -17,6 +19,7 @@ export const P2Pcard=()=>{
                <form action={async()=>{
                  const res= await P2pTransfer({to:phone||"",amount:amount||0})
                  console.log(res?.message)
+                 setmessage(res.message)
                }}>
                  <div className="min-w-72 pt-2">
                     <Text placeholder={"Number"} label="Number" id="phone" type="tel" onChange={()=>{}} onChanges={(value) => {
@@ -25,6 +28,7 @@ export const P2Pcard=()=>{
                     <Text placeholder={"Amount"} label="Amount" id="amount" setmessage="Amount should be greater than 0" min={0} onChanges={()=>{}} onChange={(value) => {
                         setamount(value)
                     }} />
+                    <div className={`text-center  ${message==="Transfer successful"?"text-green-600":" text-red-600"}`}>{message?.length?message:undefined}</div>
                     <div className="pt-4 flex justify-center">
                         <Button type={"submit"}>Send</Button>
                     </div>
--- packages/ui/src/Text.tsx
+++ packages/ui/src/Text.tsx
@@ -23,7 +23,7 @@ export const Text = ({ label, placeholder, onChange, id, setmessage, type = "num
                 min={type === "number" ? min??1 : undefined}
                 minLength={type === "tel" ? 10 : undefined}
                 maxLength={type === "tel" ? 10 : undefined}
-                pattern={type === "tel" ? "[0-9]*" : undefined}
+                pattern={type === "tel" ? "[0-9]{10}" : undefined}
                 className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none outline-hidden bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
                 onChange={(e) => {
                     if (type === "number") {
