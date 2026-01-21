839b044fdb379a5c72929fc0c38a3c70bd740afd
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-21 07:33:13 +0530

    added getransfer function for taking array of all transfer from db

--- apps/web/app/(dashboard)/p2p/page.tsx
+++ apps/web/app/(dashboard)/p2p/page.tsx
@@ -1,8 +1,24 @@
+import { getServerSession } from "next-auth";
 import { P2Pcard } from "../../../components/P2Pcard";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { prisma } from "@repo/db";
 
 export default function P2P() {
-    function gettransfer() {
-        
+   async function gettransfer() {
+        const session=await getServerSession(authOptions);
+        const user=session.user.id;
+        if(!user) return {messsage:"unautheticated user"};
+        const trasfers=await prisma.p2ptransfer.findMany({
+            where:{
+                fromuserID:user
+            },
+            select:{
+                startdate:true,
+                amount:true
+            }
+        })
+        console.log(trasfers)
     }
+    gettransfer()
     return <div><P2Pcard/></div>
 }
