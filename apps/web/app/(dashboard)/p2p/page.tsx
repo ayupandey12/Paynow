e9cdc5777be8ef9cde531d277b0db14348a81178
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-21 14:13:27 +0530

    make gettrasfer function out of component so i use in another place

--- apps/web/app/(dashboard)/p2p/page.tsx
+++ apps/web/app/(dashboard)/p2p/page.tsx
@@ -3,9 +3,7 @@ import { P2Pcard } from "../../../components/P2Pcard";
 import { authOptions } from "../../api/auth/[...nextauth]/route";
 import { prisma } from "@repo/db";
 import { Transfercard } from "../../../components/TransferCard";
-
-export default async function P2P() {
-   async function gettransfer() {
+ async function gettransfer() {
         const session=await getServerSession(authOptions);
         const user=session.user.id;
         const transfers=await prisma.p2ptransfer.findMany({
@@ -22,6 +20,8 @@ export default async function P2P() {
         })
         return transfers
     }
+export default async function P2P() {
+  
     const Transfer=await gettransfer()
     return <div className="w-screen">
             <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
