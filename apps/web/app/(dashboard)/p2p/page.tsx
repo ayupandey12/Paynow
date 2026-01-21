c80ff3a33c6a980b52ff2facc06b18bd30bb606c
ayupandey12 <ayushpandey9802@gmail.com>
2026-01-21 07:23:05 +0530

    added transfer card for recent transfer in components

--- apps/web/app/(dashboard)/p2p/page.tsx
+++ apps/web/app/(dashboard)/p2p/page.tsx
@@ -1,5 +1,8 @@
 import { P2Pcard } from "../../../components/P2Pcard";
 
 export default function P2P() {
+    function gettransfer() {
+        
+    }
     return <div><P2Pcard/></div>
 }
---
+++ apps/web/components/TransferCard.tsx
@@ -0,0 +1,37 @@
+import { Card } from "@repo/ui/card"
+
+interface transfer{
+    startdate:Date,
+    amount:number
+}
+export const Transfercard=({transfer}:{transfer:transfer[]})=>{
+     if(!transfer.length) return <Card title="Recent Transactions">
+                 <div className="text-center pb-8 pt-8">
+                     No Recent transactions
+                 </div>
+             </Card>
+    
+    return <Card title="Recent Transactions">
+          <div className=" pt-2">
+              {transfer.map(t=><div key={Math.random()} className="flex justify-between">
+                          <div>
+                              <div className="text-sm">
+                                  TRANSFERED INR
+                              </div>
+                              <div className="text-slate-600 text-xs">
+                                  {t.startdate.toDateString()}
+                              </div>
+                          </div>
+                               <div className="flex flex-col justify-center text-right">
+                                      <span className="font-semibold text-sm">
+                                          - Rs {t.amount / 100}
+                                      </span>
+                                      <span className={`text-[10px] uppercase font-bold text-green-600`}>
+                                          Success
+                                      </span>
+                               </div>
+                      </div>)}
+          </div>
+        
+    </Card>
+}
